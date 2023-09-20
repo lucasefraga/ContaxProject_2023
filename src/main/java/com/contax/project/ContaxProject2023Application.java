32371ce9fa9509a7fc7c4bef35d32a99478772c2
LucasefragaPC <lucas.fraga@sterz.it>
2023-09-20 17:03:59 +0200

    Initial Commit

---
+++ src/main/java/com/contax/project/ContaxProject2023Application.java
@@ -0,0 +1,13 @@
+package com.contax.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaxProject2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaxProject2023Application.class, args);
+	}
+
+}
